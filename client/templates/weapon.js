ca00ff2a3f9779c8daa2a16f92d810c759c26b01
iggelt <iggelt@ex.ua>
2016-08-19 00:01:18 +0300

    3

--- client/templates/weapon.js
+++ client/templates/weapon.js
@@ -1,13 +1,15 @@
 Template.weapon.helpers({
 	attributes: function(){
 		var attributes = {};
-		attributes.width = 500 + "px";
-		Meteor.setInterval(function(){
-			attributes.width = 100 + "px";
-		},5000);
 		
 		
 		return attributes;
 	}
 
 });
+Template.weapon.events({
+	'click': function(e){
+		e.preventDefault();
+		alert('ssss');
+	}
+})
