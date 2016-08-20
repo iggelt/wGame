5c4a4d24ae108d6b6fc3738d8b2b4b224b6e5747
iggelt <iggelt@ex.ua>
2016-08-20 11:28:57 +0300

    5

--- client/templates/weapon.js
+++ client/templates/weapon.js
@@ -1,33 +1,4 @@
-var objectsLeft=0;
-var objectsRight=0;
-
 Template.weapon.helpers({
-	attributes: function(){
-		var attributes = {};
-		
-		
-		return attributes;
-	},
-	top: function(){
-		return Math.round(Math.random()*100);
-	},
-	left: function(){
-		var rezult=0;
-		if(Math.max(objectsLeft,objectsRight)==objectsLeft){
-			rezult=100;
-			objectsRight++;
-		}else{
-			rezult=-100;
-			objectsLeft++;
-		}
-		return rezult;
-	},
-	duration: function(){
-		return 20;
-	},
-	delay: function(){
-		return 10;
-	}
 
 });
 Template.weapon.events({
